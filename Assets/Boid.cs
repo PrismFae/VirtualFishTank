7a3191a6b311e9fe98551f793863e17a4f709990
PrismFae <sky0522.c@icloud.com>
2025-05-14 09:34:22 -0400

    Lab Stuff

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,4 +2,8 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    private void Awake()
+    {
+        GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
+    }
 }
