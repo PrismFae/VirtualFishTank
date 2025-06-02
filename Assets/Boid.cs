a02113845f898ff54f88be64e389e4807e5c3f82
PrismFae <sky0522.c@icloud.com>
2025-06-02 19:51:25 -0400

    Lab 2

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,8 +1,89 @@
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Boid : MonoBehaviour
 {
     public Rigidbody rb;
+    
+    public float speed; // Current speed of the boid
+    public float accelMax = 3; // Max accel of the boid
+    public float speedMax = 4; // Max speed of the boid
+
+    public Vector3 currentLinearAcceleration = Vector3.zero; // Current velocity of the boid
+
+    public Vector3 Seek(Vector3 target, float magnitude)
+    {
+        // Get displacement vector to target
+        Vector3 toTarget = target - transform.position;
+
+        // Normalize to get direction to the target
+        Vector3 toTargetNormalized = toTarget.normalized;
+        
+        // Determine acceleration with a given magnitude
+        Vector3 accel = toTargetNormalized * magnitude;
+
+        return accel;
+    }
+
+    public Vector3 Pursue(Vector3 target, float acceleration, float desiredSpeed)
+    {
+        // Get displacement vector to target
+        Vector3 toTarget = target - transform.position;
+
+        // Normalize to get direction to the target
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        // Determine desired velocity -- towards target
+        Vector3 desiredVelocity = toTargetNormalized * desiredSpeed;
+
+        // Determine required change in veleocity between current and desired velocity
+        Vector3 deltaVel = desiredVelocity - rb.linearVelocity;
+
+        Vector3 accel = deltaVel.normalized * acceleration; 
+        return accel;
+    }
+
+    public Vector3 Arrive(Vector3 target, float acceleration, float arriveInnerRadius, float arriveOuterRadius)
+    {
+        // Get displacement vector to target
+        Vector3 toTarget = target - transform.position;
+
+        // Calculate distance to target
+        float distance = toTarget.magnitude;
+        distance -= arriveInnerRadius; // Subtract inner radius to get distance outside the inner radius
+        if (distance < 0) distance = 0; 
+
+        // Get direction to target
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        // Get desired velocity 
+        Vector3 desiredVelocity = toTargetNormalized * speed;
+
+        /* A percentage of the distance to the target if number is greater than one, object is outside the radius
+        if number is zero, object is at the target */
+        float distancePercent = distance / arriveOuterRadius;
+
+        // // Keep it in range
+        // if (distancePercent > 1) distancePercent = 1; not needed due to statements below
+
+        // Scale desired velocity based on distance
+        if (distance < arriveInnerRadius)
+        {
+            // If inside the inner radius, slow down to zero
+            desiredVelocity = Vector3.zero;
+            DebugDrawing.DrawCircle(transform.position, Quaternion.Euler(90,0,0), arriveInnerRadius, 8, Color.red, Time.fixedDeltaTime); // Draw inner radius circle
+        }
+        else if (distance < arriveOuterRadius)
+        {
+            // If outside the outer radius, scale down the desired velocity
+            desiredVelocity *= distancePercent;
+            DebugDrawing.DrawCircleDotted(transform.position, Quaternion.Euler(90,0,0), arriveOuterRadius, 16, 0.1f, 0.05f, Color.red, Time.fixedDeltaTime); // Draw outer radius circle
+        }
+        desiredVelocity *= distancePercent;
+
+        return desiredVelocity; // Placeholder return value
+    }
+
     private void Awake()
     {
         // Randomize the color of the boid
@@ -16,11 +97,70 @@ public class Boid : MonoBehaviour
     public void Update()
     {
         AlignToVelocity();
+        Debug.DrawLine(transform.position, transform.position + rb.linearVelocity, Color.red); // Draw a line to the target
     }
 
     // Align to the velocity of the boid
     public void AlignToVelocity()
     {
         transform.forward = Vector3.RotateTowards(transform.forward, rb.linearVelocity.normalized, Mathf.Deg2Rad * 1800 * Time.deltaTime, 100);
+    }
+    
+    public Vector3 ObstacleAvoidance(float lookaheadDistance, float acceleration)
+    {
+        Vector3 accelOut = Vector3.zero; 
+
+        // Whiskers left and right
+        Ray whiskerRight = new Ray(transform.position, Quaternion.AngleAxis(18, transform.right) * transform.forward); // Rotates forward vector by 10 degrees to the right
+
+        Ray whiskerLeft = new Ray(transform.position, Quaternion.AngleAxis(-18, transform.right) * transform.forward); // Rotates forward vector by 10 degrees to the left
+
+        RaycastHit hitInfoRight;
+        RaycastHit hitInfoLeft;
+
+        // Check if the whiskers hit an obstacle
+        bool didHitRight = Physics.Raycast(whiskerRight, out hitInfoRight, lookaheadDistance);
+        bool didHitLeft = Physics.Raycast(whiskerLeft, out hitInfoLeft, lookaheadDistance);
+
+        if (didHitRight)
+        {
+            // Turn left
+            accelOut = transform.right * acceleration; // Apply acceleration to the left
+
+            Debug.DrawLine(whiskerRight.origin, hitInfoRight.point, Color.red); // Draw a line to the hit point
+        }
+        else
+        {
+            Debug.DrawRay(whiskerRight.origin, whiskerRight.direction * lookaheadDistance, Color.yellow); // Draw the right whisker
+        }
+
+        if (didHitLeft)
+        {
+            // Turn right
+            accelOut = -transform.right * acceleration; // Apply acceleration to the right
+
+            Debug.DrawLine(whiskerLeft.origin, hitInfoLeft.point, Color.red); // Draw a line to the hit point
+        } else
+        {
+            Debug.DrawRay(whiskerLeft.origin, whiskerLeft.direction * lookaheadDistance, Color.yellow); // Draw the left whisker
+        }
+
+        return accelOut; // No avoidance needed
+    }
+
+    private void FixedUpdate()
+    {
+        speed = rb.linearVelocity.magnitude;
+
+        // Limits speed
+        if (speed > speedMax)
+        {
+            rb.linearVelocity = rb.linearVelocity * speedMax / speed;
+        }
+
+        // Orient boid towards velocity
+        // If statement to avoid stuttering
+        // Lerp to smoothen alignment
+        if (rb.linearVelocity.sqrMagnitude > 0.01f) transform.forward = Vector3.Lerp(transform.forward, rb.linearVelocity, 0.1f);
     }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    [SerializeField] GameObject foodPrefab; 
+    [SerializeField] GameObject rockPrefab; // Prefab for obstacles
+    private GameObject targetObject; 
     public GameObject boidPrefab;
     public List<Boid> boids = null;
+    public List<GameObject> rocks = null; // List of obstacles
     public int boidsToSpawn = 10;
 
     // To select 
@@ -22,8 +26,10 @@ public class BoidSimulationControl : MonoBehaviour
 
     private void Start()
     {
+        targetObject = GameObject.Find("Target");
+
         // Spawn boids
-        for(int i = 0; i < boidsToSpawn; i++)
+        for (int i = 0; i < boidsToSpawn; i++)
         {
             // Random position and rotation
             Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0, 1.4f), Random.Range(-.9f, .9f));
@@ -33,7 +39,123 @@ public class BoidSimulationControl : MonoBehaviour
             boids.Add(spawnedBoid.GetComponent<Boid>()); // List of boids
 
             spawnedBoid.transform.localScale *= Random.Range(.9f, 3f); // Random size
+
+            // Randomize properties of each boid within range
+            spawnedBoid.GetComponent<Boid>().speed = Random.Range(.5f, 2.5f);
+            spawnedBoid.GetComponent<Boid>().accelMax = Random.Range(1f, 2f);
+            spawnedBoid.GetComponent<Boid>().speedMax = Random.Range(1f, 2.3f);
         }
+    }
+
+    private void SeekModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            // Call the seek function on each boid to calculate an acceleration vector
+            Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].accelMax);
+
+            if (Input.GetMouseButton(0)) // If left mouse button is pressed
+            {
+                boids[i].currentLinearAcceleration += accel; // Apply acceleration 
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+            else if (Input.GetMouseButton(1)) // If right mouse button is pressed
+            {
+                boids[i].currentLinearAcceleration -= accel; // Apply negative acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.red); // Draw negative acceleration
+            }
+        }
+    }
+
+    private void PursueModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            // Call the pursue function on each boid to calculate an acceleration vector
+            Vector3 accel = boids[i].Pursue(targetObject.transform.position, boids[i].accelMax, boids[i].speedMax);
+            if (Input.GetMouseButton(0)) // If left mouse button is pressed
+            {
+                boids[i].currentLinearAcceleration += accel; // Apply acceleration 
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+            else if (Input.GetMouseButton(1)) // Evade mode
+            {
+                boids[i].currentLinearAcceleration -= accel; // Apply negative acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.red); // Draw negative acceleration
+            }
+        }
+    }
+
+    private void SpawnFood()
+    {
+        Instantiate(foodPrefab, targetObject.transform.position, Random.rotation); // spawn food at targetObject position
+    }
+    
+    private void SpawnObstacle()
+    {
+        Vector3 spawnPos = targetObject.transform.position; // Get the position of the target object
+        spawnPos.y -= 0.5f; // Set the y position to 0.5 to avoid spawning below the ground
+        GameObject spawnedRock = Instantiate(rockPrefab, spawnPos, Random.rotation); // spawn obstacle at targetObject position
+        rocks.Add(spawnedRock); // Add the spawned rock to the list of rocks
+    }
+
+    private void FoodArrivalBehaviour()
+    {
+        // Have all boids seek food
+        for (int i = 0; i < boids.Count; i++)
+        {
+            float foodSeekRadius = 0.8f; // Radius to seek food
+            Collider[] colliders = Physics.OverlapSphere(boids[i].transform.position, foodSeekRadius);
+
+            Food closestFood = null; // Variable to store the closest food
+            float closestFoodDistance = float.PositiveInfinity; // Initialize closest food distance to infinity
+
+            foreach (Collider collider in colliders)
+            {
+                Food food = collider.GetComponent<Food>(); // Check if the collider has a Food component
+                if (food != null) // seeks food if not null
+                {
+                    float distanceToFood = Vector3.Distance(food.transform.position, boids[i].transform.position); // Calculate distance to food
+                    if (distanceToFood < closestFoodDistance) // If left mouse button is pressed
+                    {
+                        closestFoodDistance = distanceToFood; // Set the closest food
+                        closestFood = food; // Set the closest food
+                    }
+                }
+            }
+
+            if (closestFood != null)
+            {
+                // If a food is found, apply the seek acceleration
+                Vector3 accel = boids[i].Arrive(closestFood.transform.position, boids[i].accelMax, 0.17f, 0.38f);
+                boids[i].currentLinearAcceleration += accel;
+                Debug.DrawRay(boids[i].transform.position, boids[i].currentLinearAcceleration, Color.green); // Draw acceleration towards food
+
+                // Check if the boid is close enough to the food
+                if (closestFoodDistance < 0.12f)
+                {
+                    // If the boid is close enough to the food, destroy the food object
+                    Destroy(closestFood.gameObject); // Destroy the food object after consumption
+                }
+            }
+        }
+    }
+
+    private void ResetSimulation()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            Destroy(boids[i].gameObject); // Destroy all boids
+        }
+        boids.Clear(); // Clear the list of boids
+
+        for (int i = 0; i < rocks.Count; i++)
+        {
+            Destroy(rocks[i]); // Destroy all obstacles
+        }
+        rocks.Clear(); // Clear the list of obstacles
+        
+        Start(); // Restart the simulation by calling Start method
     }
 
     private void Update()
@@ -54,6 +176,66 @@ public class BoidSimulationControl : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             controlMode = ControlMode.Obstacle;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetSimulation();
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            boids[i].currentLinearAcceleration = Vector3.zero; // Reset acceleration for each boid each cycle
+        }
+
+        foreach (Boid boid in boids)
+        {
+            // Obstacle avoidance
+            boid.currentLinearAcceleration = boid.ObstacleAvoidance(0.6f, boid.accelMax);
+        }
+
+        switch (controlMode)
+        {
+            case ControlMode.Seek:
+                SeekModeControl();
+                break;
+            case ControlMode.Pursue:
+                PursueModeControl();
+                break;
+            case ControlMode.Food:
+                if (Input.GetMouseButtonDown(0)) // If left mouse button is pressed
+                {
+                    SpawnFood(); // Spawn food at targetObject position
+                }
+                break;
+            case ControlMode.Obstacle:
+                if (Input.GetMouseButtonDown(0)) // If left mouse button is pressed
+                {
+                    SpawnObstacle(); // Spawn obstacle at targetObject position
+                }
+                break;
+        }
+        
+        FoodArrivalBehaviour();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo; // Will store information about the intersection if any
+        bool didHit = Physics.Raycast(ray, out hitInfo, 100);
+
+        if (didHit)
+        {
+            targetObject.transform.position = hitInfo.point; // Move the target to the hit point
+        }
+
+        // Enforce acceleration limit and apply velocity change
+        for (int i = 0; i < boids.Count; i++)
+        {
+            boids[i].currentLinearAcceleration = Vector3.ClampMagnitude(boids[i].currentLinearAcceleration, boids[i].accelMax);
+
+            boids[i].rb.linearVelocity += boids[i].currentLinearAcceleration * Time.fixedDeltaTime;
         }
     }
 }
